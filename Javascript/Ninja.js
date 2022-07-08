e8a2124b7d0b5a50f08522322a795f7709e8231c
Shyam Patel <shyam.psu.lv@gmail.com>
2022-07-08 10:31:14 -0400

    Extend the Ninja class and create the Sensei class.

--- Javascript/Ninja.js
+++ Javascript/Ninja.js
@@ -1,28 +1,45 @@
 class Ninja {
-    constructor(name, health = 0){
+    constructor(name, health = 0, speed = 3, strength = 3){
         this.name = name;
         this.health = health;
-        this.speed = 3;
-        this.strength = 3;
+        this.speed = speed;
+        this.strength = strength;
     }
 
     sayName(){
         console.log(this.name);
     }
     showStats(){
-        console.log("Name: ",this.name);
-        console.log("Health: ",this.health);
-        console.log("Speed: ",this.speed);
-        console.log("Strength: ",this.strength);
+        console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
     }
     drinkSake(){
         this.health += 10;
     }
 }
 
+class Sensei extends Ninja {
+    constructor(name){
+        super(name, 200, 10, 20);
+        this.wisdom = 10;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+    
+}
 
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+//example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.showStats();
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+//"Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
