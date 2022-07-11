6d636b9d509d3ece6db50ece707af0762af4e50b
Shyam Patel <shyam.psu.lv@gmail.com>
2022-07-11 16:08:41 -0400

    PropItUp with birthday button that increments age

---
+++ React/putting-it-together/src/components/PersonCard.jsx
@@ -0,0 +1,27 @@
+import {Component} from 'react'
+class PersonCard extends Component {  
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+
+    incrementAge = () => {
+        this.setState({ age: this.state.age +1});
+    }
+
+    render() {  
+        return (
+                <div>
+                    <h1>{ this.props.lastName }, { this.props.firstName } </h1>
+                    <p>Age: { this.state.age }</p>
+                    <p>Hair Color: { this.props.hairColor }</p>
+                    <button onClick={ this.incrementAge }>Birthday Button for { this.props.firstName } { this.props.lastName }</button> 
+                </div>
+                );    
+            }
+}
+    export default PersonCard;
+    
