2974c16cd4e103d7834fe6c341ae8c2e43d12daf
Shyam Patel <shyam.psu.lv@gmail.com>
2022-07-11 05:54:56 -0400

    Create a new React project using create-react-app.

---
+++ React/firstjsx/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DojoList from './components/dojolist'
+
+function App() {
+  return (
+    <div className="App">
+      <DojoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/firstjsx/src/components/dojolist.jsx
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+class DojoList extends Component {    
+        render() {        
+            return (            
+                    <>
+                        <h1>Hello Dojo!</h1>
+                        <h2>Things I need to do:</h2>
+                        <ul>
+                            <li>Learn React</li>
+                            <li>Not be Sick</li>
+                        </ul>
+                    </>
+            );    
+        }
+    }
+    export default DojoList;
+    
